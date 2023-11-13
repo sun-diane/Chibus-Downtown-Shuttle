efd61b2aab08d382b9aabaa201e986ae8580540e
sun-diane Sun <dianesun1@gmail.com>
2023-11-13 23:53:58 +0000

    Terminates at 10 PM CST

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import time
+from datetime import datetime
+import pytz
 from get_data import get_vehicle_data
  
 #this bus route runs M-F, 6:30AM to 10PM 
 def main():
 
+    cst_timezone = pytz.timezone('America/Chicago') 
+    current_date = datetime.now(cst_timezone).date() #get current date
+    stop_time = cst_timezone.localize(datetime.combine(current_date, datetime.min.time())) 
+    stop_time = stop_time.replace(hour=22, minute=30)  #set stop at 10PM on current date
+
     while True:
 
         get_vehicle_data()
-        time.sleep(180) #wait 3 minute
-        i = i + 1
+        current_time_cst = datetime.now(cst_timezone) #get current time
+        print(current_time_cst)
+        
+        if current_time_cst > stop_time:
+            print("The current time is past 10:30 PM CST. Exiting the script.")
+            sys.exit(0)  # Success!
+
+        else:
+            time.sleep(180) #wait 3 minute
 
 main()
